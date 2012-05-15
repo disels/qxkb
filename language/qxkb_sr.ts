41b607a3c4becfddddc96397350ac39a27446e8d
disels <disels@gmail.com>
2012-05-15 14:23:14 +0000

    - change XKeycodeToKeysym(Display*, KeyCode, int) to XkbKeycodeToKeysym(Display*, KeyCode, int,int) - add serbian translation files

---
+++ language/qxkb_sr.ts
@@ -0,0 +1,388 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sr_RS">
+<context>
+    <name>DstLayoutModel</name>
+    <message>
+        <location filename="../src/datamodels.cpp" line="110"/>
+        <source>Layout Name</source>
+        <translation>Име распореда</translation>
+    </message>
+    <message>
+        <location filename="../src/datamodels.cpp" line="110"/>
+        <source>Map</source>
+        <translation>Мапа</translation>
+    </message>
+    <message>
+        <location filename="../src/datamodels.cpp" line="110"/>
+        <source>Variant</source>
+        <translation>Варијанта</translation>
+    </message>
+    <message>
+        <location filename="../src/datamodels.cpp" line="110"/>
+        <source>Label</source>
+        <translation>Етикета</translation>
+    </message>
+</context>
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="33"/>
+        <location filename="../build/ui_kswconfig.h" line="558"/>
+        <source>Layout</source>
+        <translation>Распоред</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="38"/>
+        <location filename="../build/ui_kswconfig.h" line="560"/>
+        <source>Switching options</source>
+        <translation>Опције пребацивања</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="43"/>
+        <location filename="../build/ui_kswconfig.h" line="562"/>
+        <source>XKB options</source>
+        <translation>ИксКБ опције</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="72"/>
+        <location filename="../build/ui_kswconfig.h" line="569"/>
+        <source>&amp;Enable keyboard layouts</source>
+        <translation>&amp;Омогући распореде тастатуре</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="82"/>
+        <location filename="../build/ui_kswconfig.h" line="573"/>
+        <source>Indicator only</source>
+        <translation>Само индикатор</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="92"/>
+        <location filename="../build/ui_kswconfig.h" line="577"/>
+        <source>Disable keyboard layouts</source>
+        <translation>Онемогући распореде тастатуре</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="105"/>
+        <location filename="../build/ui_kswconfig.h" line="578"/>
+        <source>Indicator Options</source>
+        <translation>Опције индикатора</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="117"/>
+        <location filename="../build/ui_kswconfig.h" line="580"/>
+        <source>Shows country flag on background of layout name in tray icon</source>
+        <translation>Приказује заставицу земље на позадини имена распореда у системској касети</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="120"/>
+        <location filename="../build/ui_kswconfig.h" line="582"/>
+        <source>Show country flag</source>
+        <translation>Прикажи заставицу земље</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="130"/>
+        <location filename="../build/ui_kswconfig.h" line="583"/>
+        <source>Show indicator for single layout</source>
+        <translation>Прикажи индикатор за само један распоред</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="140"/>
+        <location filename="../build/ui_kswconfig.h" line="585"/>
+        <source>&lt;h1&gt;Keyboard Layout&lt;/h1&gt; Here you can choose your keyboard layout and model. The &apos;model&apos; refers to the type of keyboard that is connected to your computer, while the keyboard layout defines &quot;which key does what&quot; and may be different for different countries.</source>
+        <translation>&lt;h1&gt;Распоред тастатуре&lt;/h1&gt; Овде можете да изаберете модел и распоред ваше тастатуре. „Модел“ се односи на тип тастатуре која је прикључена на ваш рачунар, док распоред одређује „шта ради одређени тастер“ и може бити различит за различите земље.</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="155"/>
+        <location filename="../build/ui_kswconfig.h" line="588"/>
+        <source>Keyboard &amp;model:</source>
+        <translation>&amp;Модел тастатуре:</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="171"/>
+        <location filename="../build/ui_kswconfig.h" line="590"/>
+        <source>Here you can choose a keyboard model. This setting is independent of your keyboard layout and refers to the &quot;hardware&quot; model, i.e. the way your keyboard is manufactured. Modern keyboards that come with your computer usually have two extra keys and are referred to as &quot;104-key&quot; models, which is probably what you want if you do not know what kind of keyboard you have.
+</source>
+        <translation>Овде можете да изаберете модел тастатуре. Ово подешавање је независно од вашег распореда тастатуре и односи се на „хардверски“ модел, тј. начин на који је тастатура произведена. Модерне тастатуре обично имају два додатна тастера и означавају се као модели „104-тастера“, што је вероватно она опција која вама треба ако не знате који тип тастатуре имате.</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="194"/>
+        <location filename="../build/ui_kswconfig.h" line="593"/>
+        <source>Available layouts:</source>
+        <translation>Доступни распореди:</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="207"/>
+        <location filename="../ui/kswconfig.ui" line="244"/>
+        <location filename="../ui/kswconfig.ui" line="301"/>
+        <location filename="../ui/kswconfig.ui" line="321"/>
+        <location filename="../ui/kswconfig.ui" line="458"/>
+        <location filename="../ui/kswconfig.ui" line="485"/>
+        <location filename="../ui/kswconfig.ui" line="506"/>
+        <location filename="../build/ui_kswconfig.h" line="594"/>
+        <location filename="../build/ui_kswconfig.h" line="595"/>
+        <location filename="../build/ui_kswconfig.h" line="597"/>
+        <location filename="../build/ui_kswconfig.h" line="598"/>
+        <location filename="../build/ui_kswconfig.h" line="615"/>
+        <location filename="../build/ui_kswconfig.h" line="621"/>
+        <location filename="../build/ui_kswconfig.h" line="624"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="257"/>
+        <location filename="../build/ui_kswconfig.h" line="596"/>
+        <source>Active layouts:</source>
+        <translation>Активни распореди:</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="338"/>
+        <location filename="../build/ui_kswconfig.h" line="599"/>
+        <source>Layout variant:</source>
+        <translation>Варијанта распореда:</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="354"/>
+        <location filename="../build/ui_kswconfig.h" line="601"/>
+        <source>Here you can choose a variant of selected keyboard layout. Layout variants usually represent different key maps for the same language. For example, Ukrainian layout might have four variants: basic, winkeys (as in Windows), typewriter (as in typewriters) and phonetic (each Ukrainian letter is placed on a transliterated latin one).
+</source>
+        <translation>Овде можете да изаберете варијанту овог распореда. Варијанте распореда обично представљају различите мапе тастера за исти језик. На пример, украјински распоред може да има четири варијанте: основну, Вин тастере (као у Виндоузу), писану (као на писаћој машини) и фонетску (свако слово украјинског је смештено на пресловљено латинско).</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="362"/>
+        <location filename="../build/ui_kswconfig.h" line="604"/>
+        <source>Label:</source>
+        <translation>Етикета:</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="387"/>
+        <location filename="../ui/kswconfig.ui" line="615"/>
+        <location filename="../build/ui_kswconfig.h" line="605"/>
+        <location filename="../build/ui_kswconfig.h" line="635"/>
+        <source>Command:</source>
+        <translation>Наредба:</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="403"/>
+        <location filename="../build/ui_kswconfig.h" line="607"/>
+        <source>This is the command which will be executed to initialize configured layouts.</source>
+        <translation>Ово је наредба која ће бити извршена да би се покренули подешени распореди.</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="432"/>
+        <location filename="../build/ui_kswconfig.h" line="609"/>
+        <source>Shortcuts for switching layout</source>
+        <translation>Пречице за пребацивање распореда</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="438"/>
+        <location filename="../build/ui_kswconfig.h" line="610"/>
+        <source>Main shortcuts:</source>
+        <translation>Главне пречице:</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="448"/>
+        <location filename="../build/ui_kswconfig.h" line="612"/>
+        <source>This is a shortcut for switching layouts which is handled by X.org. It allows modifier-only shortcuts.</source>
+        <translation>Ово је пречица за пребацивање распореда којим рукује Икс.орг. Дозвољава само модификаторске пречице.</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="451"/>
+        <location filename="../ui/kswconfig.ui" line="478"/>
+        <location filename="../ui/kswconfig.ui" line="499"/>
+        <location filename="../build/ui_kswconfig.h" line="614"/>
+        <location filename="../build/ui_kswconfig.h" line="620"/>
+        <location filename="../build/ui_kswconfig.h" line="623"/>
+        <source>None</source>
+        <translation>Нема</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="465"/>
+        <location filename="../build/ui_kswconfig.h" line="616"/>
+        <source>3d level shortcuts:</source>
+        <translation>Пречице за 3-и ниво:</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="475"/>
+        <location filename="../build/ui_kswconfig.h" line="618"/>
+        <source>This is a shortcut for switching to a third level of the active layout (if it has one) which is handled by X.org. It allows modifier-only shortcuts.</source>
+        <translation>Ово је пречица за пребацивање на трећи ниво активног распореда (ако га има) којим рукује Икс.орг. Дозвољава само модификаторске пречице.</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="492"/>
+        <location filename="../build/ui_kswconfig.h" line="622"/>
+        <source>Alternative shortcut:</source>
+        <translation>Алтернативна пречица:</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="519"/>
+        <location filename="../build/ui_kswconfig.h" line="625"/>
+        <source>Switshing policy</source>
+        <translation>Начин пребацивања</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="525"/>
+        <location filename="../build/ui_kswconfig.h" line="626"/>
+        <source>&amp;Global</source>
+        <translation>&amp;Опште</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="535"/>
+        <location filename="../build/ui_kswconfig.h" line="627"/>
+        <source>Desktop</source>
+        <translation>Радна површ</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="542"/>
+        <location filename="../build/ui_kswconfig.h" line="628"/>
+        <source>Application</source>
+        <translation>Програм</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="549"/>
+        <location filename="../build/ui_kswconfig.h" line="629"/>
+        <source>&amp;Window</source>
+        <translation>&amp;Прозор</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="586"/>
+        <location filename="../build/ui_kswconfig.h" line="631"/>
+        <source>Here you can set xkb extension options instead of, or in addition to, specifying them in the X11 configuration file.</source>
+        <translation>Овде можете да подесите опције ИксКБ проширења уместо или као додатак одређивању истих у фајлу подешавања Икс-а.</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="589"/>
+        <location filename="../build/ui_kswconfig.h" line="633"/>
+        <source>Xkb Options</source>
+        <translation>Опције ИксКБ-а</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="595"/>
+        <location filename="../build/ui_kswconfig.h" line="634"/>
+        <source>&amp;Reset old options</source>
+        <translation>&amp;Ресетуј старе опције</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="622"/>
+        <location filename="../build/ui_kswconfig.h" line="637"/>
+        <source>This is the command which will be executed to initialize configured options.</source>
+        <translation>Ово је наредба којом ће се покретати подешене опције.</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="14"/>
+        <location filename="../build/ui_kswconfig.h" line="553"/>
+        <source>Antico XKB</source>
+        <translation>Антико ИксКБ</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="69"/>
+        <location filename="../build/ui_kswconfig.h" line="567"/>
+        <source>Antico will configure layouts and start layout indicator</source>
+        <translation>Антико ће да подеси распореде и покрене индикатор распореда</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="79"/>
+        <location filename="../build/ui_kswconfig.h" line="571"/>
+        <source>Antico will start indicator but will use existing layout configuration</source>
+        <translation>Антико ће да покрене индикатор али ће да користи постојећа подешавања распореда</translation>
+    </message>
+    <message>
+        <location filename="../ui/kswconfig.ui" line="89"/>
+        <location filename="../build/ui_kswconfig.h" line="575"/>
+        <source>Antico will not configure or show keyboard layouts</source>
+        <translation>Антико неће подешавати или приказивати распореде тастатуре</translation>
+    </message>
+</context>
+<context>
+    <name>Keyswitch</name>
+    <message>
+        <location filename="../src/keyswitch.cpp" line="151"/>
+        <source>Configure</source>
+        <translation>Подеси</translation>
+    </message>
+</context>
+<context>
+    <name>QXKB</name>
+    <message>
+        <location filename="../src/qxkb.cpp" line="360"/>
+        <source>Configure</source>
+        <translation>Подешавање</translation>
+    </message>
+    <message>
+        <location filename="../src/qxkb.cpp" line="366"/>
+        <source>About Qt</source>
+        <translation>О Кјуту</translation>
+    </message>
+    <message>
+        <location filename="../src/qxkb.cpp" line="370"/>
+        <location filename="../src/qxkb.cpp" line="462"/>
+        <source>About QXKB</source>
+        <translation>О КјуИксКБ-у</translation>
+    </message>
+    <message>
+        <location filename="../src/qxkb.cpp" line="376"/>
+        <source>Exit</source>
+        <translation>Напусти</translation>
+    </message>
+    <message>
+        <location filename="../src/qxkb.cpp" line="463"/>
+        <source>&lt;h2&gt;QXKB, the keyboard layout switcher&lt;/h2&gt;&lt;b&gt;Version&lt;/b&gt;: %1&lt;p&gt;Gui tool to configure xkb extentions of x server.&lt;p&gt;2009-2011(c) Fedor Chelbarakh, Petr Vanek </source>
+        <translation>&lt;h2&gt;КјуИксКБ, пребацивач распореда тастатуре&lt;/h2&gt;&lt;b&gt;Издање&lt;/b&gt;: %1&lt;p&gt;Графички алат за подешавање ИксКБ проширења Икс-сервера.&lt;p&gt;© 2009-2011 Федор Челбарах, Петр Ванек</translation>
+    </message>
+</context>
+<context>
+    <name>QXKBconf</name>
+    <message>
+        <location filename="../src/xkbconf.cpp" line="109"/>
+        <location filename="../src/xkbconf.cpp" line="111"/>
+        <source>Defined</source>
+        <translation>Дефинисано</translation>
+    </message>
+    <message>
+        <location filename="../src/xkbconf.cpp" line="269"/>
+        <location filename="../src/xkbconf.cpp" line="270"/>
+        <location filename="../src/xkbconf.cpp" line="271"/>
+        <source>Default</source>
+        <translation>Подразумевано</translation>
+    </message>
+</context>
+<context>
+    <name>SrcLayoutModel</name>
+    <message>
+        <location filename="../src/datamodels.cpp" line="15"/>
+        <source>Layout Name</source>
+        <translation>Име распореда</translation>
+    </message>
+    <message>
+        <location filename="../src/datamodels.cpp" line="15"/>
+        <source>Map</source>
+        <translation>Мапа</translation>
+    </message>
+</context>
+<context>
+    <name>XKeyboard</name>
+    <message>
+        <location filename="../src/xkeyboard.cpp" line="30"/>
+        <source>This program was built against XKB extension library
+version %1.%2, but is run with the library version %3.%4.
+This may cause various problems and even result in a complete
+failure to function
+</source>
+        <translation>Овај програм је компајлиран са верзијом %1 %2 библиотеке
+ИксКБ проширења, али је покренут са верзијом %3 %4.
+Ово може проузрочити разне проблеме или чак резултовати
+да програм уопште не функционише</translation>
+    </message>
+    <message>
+        <location filename="../src/xkeyboard.cpp" line="39"/>
+        <source>The X Server does not support a compatible XKB extension.
+Either the server is not XKB-capable or the extension was disabled.
+This program would not work with this server, so it will exit now
+</source>
+        <translation>Икс-сервер не подржава компатибилно ИксКБ проширење.
+Или сервер нема подршку за ИксКБ или је оно онемогућено.
+Овај програм неће радити са овим Икс сервером па ће сад
+да се затвори</translation>
+    </message>
+</context>
+</TS>
