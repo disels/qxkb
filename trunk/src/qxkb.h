86dfc7fbee7514deebbb73c91efd6a22cdb319d6
disels <disels@870dd2fe-8633-c374-fd2d-473762d1db62>
2013-03-31 14:00:54 +0000

    - delete qxt code. This bullshit not correctly work in razor-qt. + add copyright  for *.cpp and *.h file

--- trunk/src/qxkb.h
+++ trunk/src/qxkb.h
@@ -1,3 +1,21 @@
+/***************************************************************************
+                          qxkb.h  -  description
+                             -------------------
+    begin                : Sun Sep 22 2010
+    copyright            : (C) 2010 by Phoudor Chelbarakh
+    email                : disels@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 ////////////////////////////////////////
 //  File      : qxkb.h                //
 //  Written by: disels@gmail.com      //
