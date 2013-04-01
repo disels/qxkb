5e6b617f557aeef2327c97c4cc22e49cfe57cdd6
disels@gmail.com <disels@gmail.com@870dd2fe-8633-c374-fd2d-473762d1db62>
2013-04-01 09:38:17 +0000

    - fix addres in COPYING - delete garbage

--- trunk/src/convert.h
+++ trunk/src/convert.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+                          convert.h  -  description
+                             -------------------
+    begin                : Sun Sep 22 2010
+    copyright            : (C) 2010 by Phoudor Chelbarakh
+    email                : disels@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef CONVERT_H
 #define CONVERT_H
 
--- trunk/src/defs.h
+++ trunk/src/defs.h
@@ -1,8 +1,19 @@
-////////////////////////////////////////
-//  File      : defs.h                //
-//  Written by: g_cigala@virgilio.it  //
-//  Copyright : GPL                   //
-////////////////////////////////////////
+/***************************************************************************
+                          defs.cpp  -  description
+                             -------------------
+    begin                : Sun Sep 22 2010
+    copyright            : (C) 2010 by Phoudor Chelbarakh
+    email                : disels@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef DEFS_H
 #define DEFS_H
