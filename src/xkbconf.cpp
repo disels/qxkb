ae1c6c34b69813b2be94329fe00147e9336a5eb7
Artem Vorotnikov <artem@vorotnikov.me>
2015-03-04 23:53:39 +0300

    Remove unused icons, fix icon names

--- src/xkbconf.cpp
+++ src/xkbconf.cpp
@@ -66,10 +66,10 @@ QXKBconf::QXKBconf(QWidget* parent) : QDialog(parent)
 	xkb_conf.btnRemove->setEnabled(false);
 	xkb_conf.btnUp->setEnabled(false);
 	xkb_conf.btnDown->setEnabled(false);
-	xkb_conf.btnAdd->setIcon(QIcon::fromTheme(QString("arrow-right"),QIcon(theme+"/add.png")));
-	xkb_conf.btnRemove->setIcon(QIcon::fromTheme(QString("arrow-left"),QIcon(theme+"/rem.png")));
-	xkb_conf.btnUp->setIcon(QIcon::fromTheme(QString("arrow-up"), QIcon(theme+"/up.png")));
-	xkb_conf.btnDown->setIcon(QIcon::fromTheme(QString("arrow-down"), QIcon(theme+"/down.png")));
+	xkb_conf.btnAdd->setIcon(QIcon::fromTheme(QString("go-next"),QIcon(theme+"/add.png")));
+	xkb_conf.btnRemove->setIcon(QIcon::fromTheme(QString("go-previous"),QIcon(theme+"/rem.png")));
+	xkb_conf.btnUp->setIcon(QIcon::fromTheme(QString("go-up"), QIcon(theme+"/up.png")));
+	xkb_conf.btnDown->setIcon(QIcon::fromTheme(QString("go-down"), QIcon(theme+"/down.png")));
 	if (!setStat()) return;
 	initXKBTab();
 	xkbConf->lockKeys=true;
